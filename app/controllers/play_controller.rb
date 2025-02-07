534ca624b385f6f0bea13dbefcd5b337d70a89a1
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-02-07 01:01:21 +0000

    Final Draft 1

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -2,12 +2,28 @@ class PlayController < ApplicationController
 
   def choice
     @our_choice = params.fetch(:text)
-    @opponent_choice = ["rock","paper","scissors"].sample
+    @they_choice = ["rock","paper","scissors"].sample
+    
+    @outcome = @they_choice.class
+    
+    #Rule logic
+    if @our_choice == @they_choice
+      @outcome = "tied"
+    elsif @our_choice == "rock" && @they_choice == "paper"
+        @outcome = "lost"
+    elsif @our_choice == "rock" && @they_choice == "scissors"
+        @outcome = "won"
+    elsif @our_choice == "paper" && @they_choice == "rock"
+      @outcome = "won"
+    elsif @our_choice == "paper" && @they_choice == "scissors"
+      @outcome = "lost"
+    elsif @our_choice == "scissors" && @they_choice == "rock"
+      @outcome = "lost"
+    elsif @our_choice == "scissors" && @they_choice == "paper"
+      @outcome = "won"
+    end
 
-    @result = 
     render ({:template => "pages_templates/outcome"})
-
-  
 
   end
 
