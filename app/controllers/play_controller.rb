272724e953cb225b9c4329d7f5dd6443375a3c87
1000BPM <66570754+1000BPM@users.noreply.github.com>
2025-02-07 00:48:38 +0000

    controller fixed

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,10 +1,13 @@
 class PlayController < ApplicationController
 
   def choice
+    @our_choice = params.fetch(:text)
     @opponent_choice = ["rock","paper","scissors"].sample
+
+    @result = 
     render ({:template => "pages_templates/outcome"})
 
-    @print = params.fetch
+  
 
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 get("/", {:controller => "homepage", :action => "home"})
 
 
-get("/rock", {:controller => "play", :action => "choice"})
+get("/rock", {:controller => "play", :action => "choice", :text => "rock"})
 
-get("/paper", {:controller => "play", :action => "choice"})
+get("/paper", {:controller => "play", :action => "choice", :text => "paper"})
 
-get("/scissors", {:controller => "play", :action => "choice"})
+get("/scissors", {:controller => "play", :action => "choice", :text => "scissors"})
 end
